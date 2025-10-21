253a44bb6146be3bde6769bc285a5d502a1dc065
Luan Bogo <luanhenriquebogocandido@gmail.com>
2025-10-21 15:22:26 -0300

    new update file for creating config file for independancy offline use and new code has been added for key and essential config added and offline compatibility.

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import requests
 import os
+import json
+from threading import Thread
 
-key = requests.get()
+if os.path.exists('config.ini'):
+    with ('config.ini', 'r') as config:
+        config = json.loads(config)
+else:
+    key = requests.get("https://raw.githubusercontent.com/LuGB18/tester/refs/heads/main/key.json").json()
+    essential = requests.get("https://raw.githubusercontent.com/LuGB18/tester/refs/heads/main/update.json").json()
+    essential['key'] = key['key']
+    with open('config.ini', 'w') as config:
+        json.dump(essential, config)
+
+try:
+    key = config['key']
+except Exception as e:
+    print(f'Unable to load nessesary variables, please, contact the repo owner or\npost on the troubleshoots tab on the repo:')
+    
+
+
+
